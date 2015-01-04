fa0e2a42b6575e9333707c6025bd448177d5e945
Brian McCallister <brianm@skife.org>
2015-01-04 15:08:27 -0800

    update version

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 
 func init() {
 	app := kingpin.New(os.Args[0], usage)
-	app.Version("0.3")
+	app.Version("0.4-dev")
 
 	verbose = app.Flag("verbose", "Enable verbose output").Short('v').Bool()
 	uname = app.Flag("user", "Use current $USER for superuser").Short('u').Bool()
